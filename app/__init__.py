a71672da07d8fcc6f1816486c3f389473e2afb4c
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-08-09 11:14:21 +0800

    chore: update origins
    add the frontend url in cors origins

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 from .routes import register_routes
+import os
+
 def create_app(): 
     app = Flask(__name__)
     
@@ -12,7 +14,7 @@ def create_app():
     load_dotenv() 
     
     #enable cors for frontend
-    CORS(app, origins=["http://localhost:5173"])
+    CORS(app, origins=["http://localhost:5173",os.environ.get('VITE_FRONTEND_URL')])
     
     #register blueprints
     register_routes(app=app)
